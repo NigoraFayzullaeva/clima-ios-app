bcee991b3cdd44203b296b633d3988d52423481c
Nigora <61627731+NigoraFayzullaeva@users.noreply.github.com>
2021-05-09 21:55:48 +0300

    ...go

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -13,6 +13,29 @@ struct WeatherManager {
     
     func fetchWeather(cityName: String){
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String){
+        // 1.Create a url
+        if let url = URL(string: urlString){
+            // Create a UrlSession
+            let session = URLSession(configuration: .default)
+            // 3. Give the session a task
+           let task = session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            // 4. Start the task
+            task.resume()
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?){
+        if error != nil {
+            print(error!)
+            return
+        }
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString )
+        }
     }
 }
